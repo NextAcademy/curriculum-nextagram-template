7df68c886754795e80a4ef2e0cc1dac044841f50
Joshua Teng <joshteng@me.com>
2019-02-09 21:01:01 +0800

    manually parse database url -  database.py

--- database.py
+++ database.py
@@ -1,21 +1,34 @@
 import os
+from urllib.parse import urlparse
 
-if os.getenv('MIGRATION', '0') == '1':
-    from playhouse.postgres_ext import PostgresqlExtDatabase
-    db = PostgresqlExtDatabase('nextagram_dev')
+def parse_db_url(database_url):
+    parsed = urlparse(database_url)
+    return {
+        'user': parsed.username,
+        'password': parsed.password,
+        'host': parsed.hostname,
+        'port': parsed.port,
+        'database': parsed.path[1:]
+    }
 
-else:
-    from playhouse.db_url import parse
-    from playhouse.pool import PooledPostgresqlExtDatabase
+def return_db():
+    db_config = parse_db_url(os.environ['DATABASE_URL'])
 
-    db_config = parse(os.environ['DATABASE_URL'])
+    if os.getenv('MIGRATION', '0') == '1':
+        from playhouse.postgres_ext import PostgresqlExtDatabase
 
-    db = PooledPostgresqlExtDatabase(
-        db_config['database'],
-        max_connections=os.getenv('DB_POOL', 5),
-        stale_timeout=os.getenv('DB_TIMEOUT', 300),  # 5 minutes.
-        user=db_config.get('user', None),
-        password=db_config.get('password', None),
-        host=db_config.get('host', 'localhost'),
-        port=db_config.get('port', '5432'))
+        return PostgresqlExtDatabase('nextagram_dev')
 
+    else:
+        from playhouse.pool import PooledPostgresqlExtDatabase
+
+        return PooledPostgresqlExtDatabase(
+            db_config['database'],
+            max_connections=os.getenv('DB_POOL', 5),
+            stale_timeout=os.getenv('DB_TIMEOUT', 300),  # 5 minutes.
+            user=db_config.get('user', None),
+            password=db_config.get('password', None),
+            host=db_config.get('host', 'localhost'),
+            port=db_config.get('port', '5432'))
+
+db = return_db()
