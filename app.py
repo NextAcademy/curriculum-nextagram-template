a16f66a32af4b0254b087e877483c3feca2288b0
Joshua Teng <joshteng@me.com>
2019-02-21 03:25:07 +0800

    change to use teardown request

--- app.py
+++ app.py
@@ -19,7 +19,9 @@ def before_request():
     db.connect()
 
 
-@app.after_request
-def after_request(response):
-    db.close()
-    return response
+@app.teardown_request
+def _db_close(exc):
+    if not db.is_closed():
+        print(db)
+        print(db.close())
+    return exc
