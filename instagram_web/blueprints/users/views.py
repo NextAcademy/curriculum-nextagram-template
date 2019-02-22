67189e67a3b9bbae73381ae46916d642e9103004
Joshua Teng <joshteng@me.com>
2019-02-22 22:09:09 +0800

    fix template namespace

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -3,12 +3,12 @@ from flask import Blueprint, render_template
 
 users_blueprint = Blueprint('users',
                             __name__,
-                            template_folder='templates/users')
+                            template_folder='templates')
 
 
 @users_blueprint.route('/new', methods=['GET'])
 def new():
-    return render_template('new.html')
+    return render_template('users/new.html')
 
 
 @users_blueprint.route('/', methods=['POST'])
