1e7d5d9fb9ca4cffc17278585f29e4703020add5
jnsy <jadensy99@gmail.com>
2019-04-01 14:19:19 +0800

    add sign up form & create new user functionality

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,19 +1,30 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from werkzeug.security import generate_password_hash
+from models.user import User
 
 users_blueprint = Blueprint('users',
                             __name__,
                             template_folder='templates')
 
-
 @users_blueprint.route('/new', methods=['GET'])
 def new():
     return render_template('users/new.html')
 
-
 @users_blueprint.route('/', methods=['POST'])
 def create():
-    pass
+    new_pw_hashed = generate_password_hash(request.form.get('new_password'))
+    u = User(username=request.form.get('new_username'),
+            first_name=request.form.get('new_firstname'),
+            last_name=request.form.get('new_lastname'),
+            email=request.form.get('new_email'),
+            password=new_pw_hashed)
+
+    if u.save():
+        flash("New user created.")
+        return redirect(url_for('users.new'))
+    else:
+        flash("Failed to create new user. Try again?")
+        return render_template('users/new.html', errors=u.errors)
 
 
 @users_blueprint.route('/<username>', methods=["GET"])
--- models/user.py
+++ models/user.py
@@ -1,9 +1,11 @@
 from models.base_model import BaseModel
 import peewee as pw
 
+# from flask_wtf import FlaskForm
+
 class User(BaseModel):
-    first_name = pw.CharField(unique=False)
-    last_name = pw.CharField(unique=False, null=True)
+    first_name = pw.CharField()
+    last_name = pw.CharField(null=True)
     username = pw.CharField(unique=True)
     email = pw.CharField(unique=True)
-    password = pw.CharField(unique=False)
+    password = pw.CharField()
