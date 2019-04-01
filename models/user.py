7738e75456439f7f3f9500adc302160f7f8de798
jnsy <jadensy99@gmail.com>
2019-04-01 14:32:52 +0800

    add error handling for duplicate username & email

--- models/user.py
+++ models/user.py
@@ -9,3 +9,12 @@ class User(BaseModel):
     username = pw.CharField(unique=True)
     email = pw.CharField(unique=True)
     password = pw.CharField()
+
+    def validate(self):
+        duplicate_username = User.get_or_none(User.username == self.username)
+        duplicate_email = User.get_or_none(User.email == self.email)
+
+        if duplicate_username:
+            self.errors.append('Username has been registered. Please try another.')
+        elif duplicate_email:
+            self.errors.append('Email has been registered. Please try another.')
