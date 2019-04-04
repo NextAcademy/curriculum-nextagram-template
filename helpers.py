546ae1edce73e60a25812ec3d1fc88376e0dd6be
seowyongtao <seowyongtao22@gmail.com>
2019-04-04 17:33:30 +0800

    added donation and send email feature

--- helpers.py
+++ helpers.py
@@ -1,8 +1,19 @@
 import boto3, botocore
 from config import S3_KEY, S3_SECRET, S3_BUCKET
+from config import MERCHANT_ID, PUBLIC_KEY,PRIVATE_KEY
+import braintree
 
 s3 = boto3.client(
    "s3",
    aws_access_key_id=S3_KEY,
    aws_secret_access_key=S3_SECRET
+)
+
+gateway = braintree.BraintreeGateway(
+    braintree.Configuration(
+        braintree.Environment.Sandbox,
+        merchant_id=MERCHANT_ID,
+        public_key=PUBLIC_KEY,
+        private_key=PRIVATE_KEY
+    )
 )
