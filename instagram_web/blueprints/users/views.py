cac7d87c2ef252ddca029d56ae954e060ce70b5f
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-05-03 15:42:11 +0800

    added method for new user to sign-up

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from models.user import User
+from werkzeug.security import generate_password_hash
 
 users_blueprint = Blueprint('users',
                             __name__,
@@ -13,7 +14,19 @@ def new():
 
 @users_blueprint.route('/', methods=['POST'])
 def create():
-    pass
+    user_name = request.form.get('user_name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    hashed_password = generate_password_hash(password)
+    newuser = User(username=user_name, email=email, password=hashed_password)
+
+    if newuser.save():
+        flash(f'Welcome {user_name}')
+        return redirect(url_for('users.new'))
+
+    else:
+        flash(f'{user_name} is already taken. Pick another')
+        return render_template('users/new.html', errors=newuser.errors)
 
 
 @users_blueprint.route('/<username>', methods=["GET"])
--- models/user.py
+++ models/user.py
@@ -3,4 +3,18 @@ import peewee as pw
 
 
 class User(BaseModel):
-    name = pw.CharField(unique=False)
+    username = pw.CharField(unique=True, null=False)
+    email = pw.CharField(unique=True, null=False)
+    password = pw.CharField(null=False)
+
+    def validate(self):
+        duplicate_user = User.get_or_none(
+            User.username == self.username)
+
+        if duplicate_user:
+            self.errors.append('Name taken')
+
+        duplicate_email = User.get_or_none(User.email == self.email)
+
+        if duplicate_email:
+            self.errors.append('An account with that email already exists')
