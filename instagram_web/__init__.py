f0888a7db4aa58ea02cfc9cffe594e8df671b8f3
Victoria Lee <vickyloon91@gmail.com>
2019-05-06 17:09:59 +0800

    added error page

--- instagram_web/__init__.py
+++ instagram_web/__init__.py
@@ -28,6 +28,11 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 @app.route("/")
 def home():
     return render_template('home.html')
