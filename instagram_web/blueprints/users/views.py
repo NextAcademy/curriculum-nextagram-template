854570db46d437eb3cd299322ce000089470d226
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-05-07 10:13:13 +0800

    added edit information link to taskbar

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -55,7 +55,12 @@ def index():
 
 @users_blueprint.route('/<id>/edit', methods=['GET'])
 def edit(id):
-    return render_template('edit_user.html', user=User.get_by_id(id))
+    user = User.get_by_id(id)
+    if current_user == user:
+        return render_template('edit_user.html')
+    else:
+        flash('You are not authorized to do this!')
+        return redirect(url_for('home'))
 
 
 @users_blueprint.route('/<id>', methods=['POST'])
@@ -66,21 +71,22 @@ def update(id):
         flash('You are not authorized to do this!')
         return render_template('edit_user.html', user=user)
 
-    new_user_name = request.form.get('new_user_name')
-    new_email = request.form.get('new_email')
-    new_password = request.form.get('new_password')
+    else:
+        new_user_name = request.form.get('new_user_name')
+        new_email = request.form.get('new_email')
+        new_password = request.form.get('new_password')
 
-# use update because using save will execute the validation in users.py
-    update_user = User.update(
-        username=new_user_name,
-        email=new_email,
-        password=new_password
-    ).where(User.id == id)
+    # use update because using save will execute the validation in users.py
+        update_user = User.update(
+            username=new_user_name,
+            email=new_email,
+            password=new_password
+        ).where(User.id == id)
 
-    if not update_user.execute():
-        flash(
-            f'Unable to update, please try again')
-        return render_template('edit_user.html', user=user)
+        if not update_user.execute():
+            flash(
+                f'Unable to update, please try again')
+            return render_template('edit_user.html', user=user)
 
-    flash('Successfully updated')
-    return redirect(url_for('home'))
+        flash('Successfully updated')
+        return redirect(url_for('home'))
