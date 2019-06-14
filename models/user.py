4736e844b07d7e08543d9ed1e931c947bd9fe2a6
Ian <fishy90@gmail.com>
2019-06-14 15:24:03 +0800

    updated User table with username, email and password. added validation

--- models/user.py
+++ models/user.py
@@ -3,4 +3,15 @@ import peewee as pw
 
 
 class User(BaseModel):
-    name = pw.CharField(unique=False)
+    username = pw.CharField(unique=True)
+    password = pw.CharField()
+    email = pw.CharField(unique=True)
+
+    def validate(self):
+        duplicate_username = User.get_or_none(User.username == self.username)
+        duplicate_email = User.get_or_none(User.email == self.email)
+
+        if duplicate_username:
+            self.errors.append('Username not unique')
+        elif duplicate_email:
+            self.errors.append('Email not unique')
