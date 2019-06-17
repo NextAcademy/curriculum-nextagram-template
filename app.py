5dbad6bd96f41e595f7cb899f9d423def2265bc4
Ian <fishy90@gmail.com>
2019-06-17 15:33:19 +0800

    basic login and session store implemented

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import config
-from flask import Flask
+from flask import Flask, session, redirect, url_for, escape, request, render_template
 from models.base_model import db
 from flask_wtf.csrf import CSRFProtect
 
@@ -11,6 +11,7 @@ web_dir = os.path.join(os.path.dirname(
 
 app = Flask('NEXTAGRAM', root_path=web_dir)
 csrf = CSRFProtect(app)
+app.secret_key = os.getenv('SECRET_KEY')
 
 if os.getenv('FLASK_ENV') == 'production':
     app.config.from_object("config.ProductionConfig")
@@ -29,3 +30,23 @@ def _db_close(exc):
         print(db)
         print(db.close())
     return exc
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return render_template('home.html', sessionstatus = session['username'])
+    return render_template('home.html', sessionstatus = None)
+    
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request,redirect, url_for, Blueprint,flash
+from flask import Flask, render_template, request, redirect, url_for, Blueprint, flash, session, escape
 from models.user import User
+from werkzeug.security import check_password_hash
 
 users_blueprint = Blueprint('users',
                             __name__,
@@ -23,6 +24,25 @@ def create():
 @users_blueprint.route('/<username>', methods=["GET"])
 def show(username):
     pass
+
+@users_blueprint.route('/', methods=['POST'])
+def signin():
+        email = request.form['email']
+        password = request.form['password']
+        u = User.get_or_none(User.email == email)
+        if u != None:
+                flash(f'User found {u.username}')
+                if check_password_hash(u.password, password):
+                        flash('Password is a match!')
+                        session['username'] = u.username
+                        return redirect(url_for('index'))
+                else:
+                        flash('But wrong password')
+                        return render_template('home.html', email=request.form['email'])
+        else:
+                flash('No such user')
+                return render_template('home.html')
+                
 
 
 @users_blueprint.route('/', methods=["GET"])
