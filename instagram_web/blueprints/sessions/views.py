f60250bac583480e7c881ac856042d268f461982
Matthew Cross <mattyc246@gmail.com>
2019-07-17 16:36:37 +0800

    log in page generated

---
+++ instagram_web/blueprints/sessions/views.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template, redirect, url_for, request
+
+sessions_blueprint = Blueprint(
+    'sessions', __name__, template_folder='templates/sessions')
+
+
+@sessions_blueprint.route('/new', methods=['GET'])
+def new():
+    return render_template('new.html')
