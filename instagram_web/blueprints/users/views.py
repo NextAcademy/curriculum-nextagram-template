26efee64ae65b29bc8c578ff13d1d5a4d92814b6
Matthew Cross <mattyc246@gmail.com>
2019-07-17 16:04:03 +0800

    user can sign up

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,9 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from werkzeug.security import generate_password_hash
+from models.user import User
 
 
 users_blueprint = Blueprint('users',
                             __name__,
                             template_folder='templates')
+
 
 @users_blueprint.route('/', methods=['POST'])
 def create():
@@ -12,7 +15,21 @@ def create():
     username = request.form.get('username')
     password = request.form.get('password')
 
-    
+    secure_password = generate_password_hash(password)
+
+    user = User(
+        full_name=full_name,
+        email=email,
+        username=username,
+        password=secure_password
+    )
+
+    if user.save():
+        flash('Successfully created new account', 'success')
+        return redirect(url_for('home'))
+
+    flash('Error creating account, please check details', 'warning')
+    return redirect(url_for('home'))
 
 
 @users_blueprint.route('/<username>', methods=["GET"])
