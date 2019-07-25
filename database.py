ef94bdb74584429f23680fc503ec9605c531db2c
Matthew Cross <41043384+mattyc246@users.noreply.github.com>
2019-07-25 22:50:53 +0800

    Revert "Feat/image upload"

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 import os
 from urllib.parse import urlparse
 
-
 def parse_db_url(database_url):
     parsed = urlparse(database_url)
     return {
@@ -12,9 +11,8 @@ def parse_db_url(database_url):
         'database': parsed.path[1:]
     }
 
-
 def return_db():
-    db_config = parse_db_url(os.getenv('DATABASE_URL'))
+    db_config = parse_db_url(os.environ['DATABASE_URL'])
 
     if os.getenv('MIGRATION', '0') == '1':
         from playhouse.postgres_ext import PostgresqlExtDatabase
@@ -32,11 +30,10 @@ def return_db():
         return PooledPostgresqlExtDatabase(
             db_config['database'],
             max_connections=os.getenv('DB_POOL', 5),
-            stale_timeout=os.getenv('DB_TIMEOUT', 3000),  # 5 minutes.
+            stale_timeout=os.getenv('DB_TIMEOUT', 300),  # 5 minutes.
             user=db_config.get('user', None),
             password=db_config.get('password', None),
             host=db_config.get('host', 'localhost'),
             port=db_config.get('port', '5432'))
-
 
 db = return_db()
