70447bbbb1768840ff1215cd7525bbb50b496da3
Ben Hails <benhails@hotmail.com>
2020-01-20 13:49:17 +0800

    Signup page with hashed pw entry but no submit functionality

--- models/user.py
+++ models/user.py
@@ -4,3 +4,6 @@ import peewee as pw
 
 class User(BaseModel):
     name = pw.CharField(unique=False)
+    username = pw.CharField(unique=True)
+    email = pw.CharField(unique=True)
+    password = pw.CharField(unique=False)
