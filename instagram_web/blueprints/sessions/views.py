42ad767afc0a9ac621e0613fe811dc1c51a26388
Ben Hails <benhails@hotmail.com>
2020-01-22 11:08:04 +0800

    Added initial login support

--- instagram_web/blueprints/sessions/views.py
+++ instagram_web/blueprints/sessions/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 from werkzeug.security import check_password_hash
 from models.user import User
+from flask_login import login_user
 
 
 sessions_blueprint = Blueprint('sessions',
@@ -21,8 +22,7 @@ def login_post():
         hashed_password = user_for_auth.password
         result = check_password_hash(hashed_password, password_to_check) # this function return True or False to result
         if result:
-            # let flask-login to take over
-            # login_user(user_for_auth)
+            login_user(user_for_auth) # let flask login take over
             session["user_id"] = user_for_auth.id
             flash('You are now logged in', 'success')
             return redirect('/')
