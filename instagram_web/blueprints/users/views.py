dc411a46caef7ac41c3949a53046587567340c93
Phoong Shen Yang <shenyang_zx@hotmail.com>
2020-02-26 18:20:04 +0800

    sign in form, flash added.
    Sign in form can check if user exists and also checks against the hashed pass. But no session saving yet.

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from models.user import User
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 import re
 
 
@@ -9,12 +9,35 @@ users_blueprint = Blueprint('users',
                             template_folder='templates')
 
 
+@users_blueprint.route('/sign_in', methods=['GET'])
+def sign_in():
+    return render_template('users/sign_in.html')
+
+
+@users_blueprint.route('/signed_in', methods=['POST'])
+def signed_in():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if User.get_or_none(User.username == username):
+        user = User.get(User.username == username)
+        hashed_pass = user.password
+        if check_password_hash(hashed_pass, password):
+            flash('ye, you is in')
+            return redirect(url_for('users.index'))
+        else:
+            flash('right name, wrong pass')
+            return render_template('users/sign_in.html')
+    else:
+        flash("bruh, you don't exist")
+        return render_template('users/sign_in.html')
+
+
 @users_blueprint.route('/new', methods=['GET'])
 def new():
     return render_template('users/new.html')
 
 
-@users_blueprint.route('/', methods=['POST'])
+@users_blueprint.route('/create', methods=['POST'])
 def create():
     em = request.form.get('email')
     us = request.form.get('username')
@@ -22,7 +45,6 @@ def create():
 
     if len(pa) > 6:
         if re.search(r'[A-Z]', pa) and re.search(r'[a-z]', pa) and re.search(r'\W', pa):
-
             hashed_pa = generate_password_hash(pa)
             new_user = User(email=em, username=us, password=hashed_pa)
 
