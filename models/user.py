f75569c570a38f707aeab7b202c4cc725bff3a19
Phoong Shen Yang <shenyang_zx@hotmail.com>
2020-02-26 11:16:41 +0800

    Fixed validation and typo.

--- models/user.py
+++ models/user.py
@@ -5,9 +5,14 @@ import peewee as pw
 class User(BaseModel):
     username = pw.CharField(unique=True, null=False)
     email = pw.CharField(unique=True, null=False)
-    password = pw.CharField(uniqie=False, null=False)
+    password = pw.CharField(unique=False, null=False)
 
     def validate(self):
-        print(
-            f"Username or email has already been registered.")
-        return True
+        duplicate_username = User.get_or_none(User.username == self.username)
+        duplicate_email = User.get_or_none(User.email == self.email)
+
+        if duplicate_username:
+            self.errors.append('Username taken. ')
+
+        if duplicate_email:
+            self.errors.append('Email has been registered.')
