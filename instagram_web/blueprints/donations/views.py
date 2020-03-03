142d4a4f763d2863fec5b88f37f106a65f866b58
Phoong Shen Yang <shenyang_zx@hotmail.com>
2020-03-03 17:38:15 +0800

    working email notifier

--- instagram_web/blueprints/donations/views.py
+++ instagram_web/blueprints/donations/views.py
@@ -2,6 +2,23 @@ from instagram_web.util.gateway import gateway
 from flask import Blueprint, render_template, request, redirect, url_for, flash, request
 from flask_login import current_user, login_required
 from models.donations import Donation
+import os
+from models.user import User
+from models.user_images import Image
+import requests
+
+mg_domain = os.environ.get('MAILGUN_DOMAIN')
+mg_api_key = os.environ.get('MAILGUN_API_KEY')
+
+
+def send_simple_message(email):
+    return requests.post(
+        f"https://api.mailgun.net/v3/{mg_domain}/messages",
+        auth=("api", f"{mg_api_key}"),
+        data={"from": f"Excited User <mailgun@{mg_domain}>",
+              "to": email,
+              "subject": "Dono",
+              "text": "Testing some Mailgun awesomness!"})
 
 
 donations_blueprint = Blueprint(
@@ -28,11 +45,12 @@ def create(image_id):
         }
     })
     if not result.is_success:
-        flash('dono did not work out.')
+        flash('dono did not work out.', 'warning')
         return redirect(request.referrer)
 
     dono = Donation(amount=amount, image_id=image_id)
-    if not dono.save():
-        flash('dono was successful. database not updated')
-
-    return render_template('donations/new.html', payment_nonce=payment_nonce, result=result)
+    if dono.save():
+        user = User.select().join(Image).where(Image.id == image_id)
+        send_simple_message(user[0].email)
+        flash(f'successfully donated {amount}', 'success')
+        return redirect(url_for('users.index'))
