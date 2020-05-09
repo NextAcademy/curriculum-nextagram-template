b87181e976a4f085d7b93017d02e6d3162f96ca5
Phoong Shen Yang <shenyang_zx@hotmail.com>
2020-05-09 19:03:08 +0800

    made new model for monopoly users. and sign up page

--- instagram_web/blueprints/monopoly/views.py
+++ instagram_web/blueprints/monopoly/views.py
@@ -1,9 +1,31 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, flash,  redirect, url_for
+from models.mon_user import Mon_User
+
 
 monopoly_blueprint = Blueprint(
     'monpoly', __name__, template_folder='templates')
 
 
 @monopoly_blueprint.route('/')
-def show():
+def index():
     return render_template('monopoly/index1.html')
+
+
+@monopoly_blueprint.route('/new')
+def new():
+    return render_template('monopoly/new.html')
+
+
+@monopoly_blueprint.route('/create', methods=['POST'])
+def create():
+    name = request.form.get('username')
+    password = request.form.get('password')
+
+    new_user = Mon_User(name=name, password=password)
+    if new_user.save():
+        flash('success!', 'success')
+        return redirect(url_for('monopoly.index'))
+
+    else:
+        flash('Did not succeed, try again.', 'danger')
+        return render_template('monopoly/new.html')
---
+++ models/mon_user.py
@@ -0,0 +1,18 @@
+from models.base_model import BaseModel
+import peewee as pw
+from flask_login import current_user
+from playhouse.hybrid import hybrid_property
+
+
+class Mon_User(BaseModel):
+    name = pw.CharField(unique=True, null=False)
+    password = pw.CharField(unique=False, null=False)
+    position = pw.IntegerField(unique=False, default=0)
+    properties = pw.TextField(null=True)
+    money = pw.IntegerField(default=0)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
