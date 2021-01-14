fecee4ae6dcf01d2b333d4aa6a612f267ed54254
Jo-Anne Tan <ancientchronicles.anne@gmail.com>
2021-01-14 15:26:58 +0800

    added user log in function and cookie

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,9 +1,41 @@
-from flask import Blueprint, flash, render_template,request,redirect,url_for
+from flask import Blueprint, flash, render_template,request,redirect,url_for,session
 from models.user import User
+#-----------------------------------------------------------------
+from werkzeug.security import check_password_hash
+#-----------------------------------------------------------------
 
 users_blueprint = Blueprint('users',
                             __name__,
                             template_folder='templates')
+
+#-----------------------------------------------------------------
+@users_blueprint.route('/login', methods=["GET"])
+def login():
+    return render_template('users/login.html')
+
+@users_blueprint.route('/auth', methods=["POST"])
+def authentication():
+    username = request.form['name']
+    password = request.form['password']
+
+    try:
+        user = User.get(name=username)
+    except:
+        flash('Username does not exist. Please try again.')
+        return redirect(url_for('users.login'))
+
+    db_password = user.password
+    match = check_password_hash(db_password,password)
+
+    if match:
+        flash("User is logged in!")
+        session["username"] = user.name
+        return redirect(url_for('home'))
+    else:
+        flash("Incorrect password. Please try again.")
+        return redirect(url_for('users.login'))
+
+#-----------------------------------------------------------------
 
 
 @users_blueprint.route('/new', methods=['GET'])
