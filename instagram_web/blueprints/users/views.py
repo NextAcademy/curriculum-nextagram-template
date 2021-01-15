20aac638179d0102352a869e5154da5b4b5d7020
Jo-Anne Tan <ancientchronicles.anne@gmail.com>
2021-01-15 21:30:57 +0800

    WIP - changing email logic in user views.py

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,14 +1,18 @@
 from flask import Blueprint, flash, render_template,request,redirect,url_for,session,abort
 from models.user import User
-#---------------------DAY2--------------------------------------------
+#---------------------DAY 2--------------------------------------------
 from flask_login import login_user,login_required,logout_user,current_user
 #-----------------------END------------------------------------------
+#---------------------DAY 3--------------------------------------------
+from werkzeug.security import check_password_hash
+#-----------------------END------------------------------------------
+
 
 users_blueprint = Blueprint('users',
                             __name__,
                             template_folder='templates')
 
-#---------------------DAY2--------------------------------------------
+#---------------------DAY 2--------------------------------------------
 @users_blueprint.route('/login', methods=["GET"])
 def login():
     return render_template('users/login.html')
@@ -91,8 +95,29 @@ def edit(id):
 
 # Method for changing email
 @users_blueprint.route('/<int:id>/update_email', methods=['POST'])
-def update_username(id):
-    pass
+def update_email(id):
+    form_email = request.form['new_email']
+    form_password=request.form['password_1']
+    user = User.get_by_id(id)
+
+    # check if password matches user
+    match = check_password_hash(user.password,form_password)
+    if not match:
+        flash("Incorrect password. Please try again")
+        return redirect(url_for('users.edit',id=user.id))
+    
+    # check if email fulfills unique requirement
+    email_check=User.get_or_none(email=form_email)
+
+    if email_check: # email is used
+        flash("This email is used. Please try a different email.")
+        return redirect(url_for('users.edit',id=user.id))
+
+    else: # create new user
+        new_user = User(name=user.name, password=form_password, email=form_email)
+        flash("Email updated!")
+        return redirect(url_for('users.edit',id=new_user.id))
+
 
 
 # ----------- END -------------------------------------------------------------
